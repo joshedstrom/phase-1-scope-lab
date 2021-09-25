a9a48b3a7425dbdb5facc305fd6f8cdfde529ce6
Joshua Hedstrom <jhhedstrom@gmail.com>
2021-09-25 15:19:41 -0400

    commit tree deeee7a08d8406e8e59dab4ff91a59bd167f40da

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'me';
+
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    window.bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    window.bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'anna'
+}
